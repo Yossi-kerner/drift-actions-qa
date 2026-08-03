1275d5c6d09a77d1bfe3800ecbd23a895d12d7e4
Yossi Kerner <yossi.kerner@env0.com>
2026-08-03 16:22:27 +0300

    bump cloudposse/label/null 0.24.1 -> 0.25.0 (moduleVersionChange seed)

--- pinned-module/main.tf
+++ pinned-module/main.tf
@@ -8,14 +8,14 @@ terraform {
 
 module "label" {
   source    = "cloudposse/label/null"
-  version   = "0.24.1"
+  version   = "0.25.0"
   namespace = "driftqa"
   name      = "pinned-module"
 }
 
 resource "null_resource" "pinned_module" {
   triggers = {
-    rev   = "m1"
+    rev   = "m2"
     label = module.label.id
   }
 }
