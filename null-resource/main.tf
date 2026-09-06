702bfaa923f080c1b686bd796439ed96f65556a9
Yossi Kerner <yossi.kerner@env0.com>
2026-09-06 15:27:49 +0300

    APO-740 QA: apply-time failure with a long message

--- null-resource/main.tf
+++ null-resource/main.tf
@@ -8,7 +8,7 @@ terraform {
 
 variable "drift_trigger" {
   type    = string
-  default = "apo740-run1"
+  default = "apo740-run3"
 }
 
 variable "secret_trigger" {
@@ -27,4 +27,14 @@ resource "null_resource" "drift_qa" {
 variable "apo603_c1" {
   type    = string
   default = "c1"
+}
+
+resource "null_resource" "apo740_failing_apply" {
+  triggers = {
+    run = "apo740-qa-3"
+  }
+
+  provisioner "local-exec" {
+    command = "echo 'APO-740 QA deliberate apply failure: this provisioner prints a very long single line so that the generated commit check description exceeds the two hundred character cap enforced at the write boundary in put-commit-check, exercising the truncation and the ANSI and box-drawing stripping together' && exit 1"
+  }
 }
